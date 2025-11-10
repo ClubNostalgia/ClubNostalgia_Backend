a4fac27b4269360253fee6e176ff1a24d6b4cd7e
AnaAguileraMorales88 <anitaaguilera@hotmail.com>
2025-11-10 15:09:34 +0100

    feat/Primer commit conexión con la base de datos

---
+++ src/main/java/com/ClubNostalgia/backend/ClubNostalgiaApplication.java
@@ -0,0 +1,13 @@
+package com.ClubNostalgia.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubNostalgiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubNostalgiaApplication.class, args);
+	}
+
+}
