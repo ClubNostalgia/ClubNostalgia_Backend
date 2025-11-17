25999e745eda8402d2fb8a1e1830d517ab152b4c
AnaAguileraMorales88 <anitaaguilera@hotmail.com>
2025-11-17 22:32:58 +0100

    Modificación en PasswordEncoder

--- src/main/java/com/ClubNostalgia/backend/ClubNostalgiaApplication.java
+++ src/main/java/com/ClubNostalgia/backend/ClubNostalgiaApplication.java
@@ -2,18 +2,11 @@ package com.ClubNostalgia.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class ClubNostalgiaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ClubNostalgiaApplication.class, args);
-	}
-	@Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder(){
-        return new BCryptPasswordEncoder();
+    public static void main(String[] args) {
+        SpringApplication.run(ClubNostalgiaApplication.class, args);
     }
-
 }
